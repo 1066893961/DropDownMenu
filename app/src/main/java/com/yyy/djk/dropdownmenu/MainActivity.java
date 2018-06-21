f867b5119eca9aaa30334d67387a74d783c8d5df
lwz <m18911450394@163.com>
2018-06-21 11:26:15 +0800

    pop demo

--- app/src/main/java/com/yyy/djk/dropdownmenu/MainActivity.java
+++ app/src/main/java/com/yyy/djk/dropdownmenu/MainActivity.java
@@ -2,16 +2,16 @@ package com.yyy.djk.dropdownmenu;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.TypedValue;
-import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.GridView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.yyydjk.library.DropDownMenu;
+import com.yyydjk.library.DropDownMenu2;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -19,19 +19,37 @@ import java.util.List;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    @InjectView(R.id.dropDownMenu) DropDownMenu mDropDownMenu;
-    private String headers[] = {"城市", "年龄", "性别", "星座"};
+    @InjectView(R.id.dropDownMenu)
+    DropDownMenu mDropDownMenu;
+    @InjectView(R.id.dropDownMenuTop)
+    DropDownMenu2 mDropDownMenuTop;
+    @InjectView(R.id.btn)
+    Button btn;
+    @InjectView(R.id.top_ll)
+    LinearLayout topLl;
+    @InjectView(R.id.all_ll)
+    LinearLayout allLl;
+    @InjectView(R.id.bottom_ll)
+    LinearLayout bottomLl;
+    @InjectView(R.id.top_tv)
+    TextView topTv;
+
+
+
+    private TextView contentView;
+    private String headers[] = {"年龄", "性别", "星座"};
     private List<View> popupViews = new ArrayList<>();
 
-    private GirdDropDownAdapter cityAdapter;
+    //    private GirdDropDownAdapter cityAdapter;
     private ListDropDownAdapter ageAdapter;
     private ListDropDownAdapter sexAdapter;
     private ConstellationAdapter constellationAdapter;
 
-    private String citys[] = {"不限", "武汉", "北京", "上海", "成都", "广州", "深圳", "重庆", "天津", "西安", "南京", "杭州"};
+    //    private String citys[] = {"不限", "武汉", "北京", "上海", "成都", "广州", "深圳", "重庆", "天津", "西安", "南京", "杭州"};
     private String ages[] = {"不限", "18岁以下", "18-22岁", "23-26岁", "27-35岁", "35岁以上"};
     private String sexs[] = {"不限", "男", "女"};
     private String constellations[] = {"不限", "白羊座", "金牛座", "双子座", "巨蟹座", "狮子座", "处女座", "天秤座", "天蝎座", "射手座", "摩羯座", "水瓶座", "双鱼座"};
@@ -48,10 +66,10 @@ public class MainActivity extends AppCompatActivity {
 
     private void initView() {
         //init city menu
-        final ListView cityView = new ListView(this);
-        cityAdapter = new GirdDropDownAdapter(this, Arrays.asList(citys));
-        cityView.setDividerHeight(0);
-        cityView.setAdapter(cityAdapter);
+//        final ListView cityView = new ListView(this);
+//        cityAdapter = new GirdDropDownAdapter(this, Arrays.asList(citys));
+//        cityView.setDividerHeight(0);
+//        cityView.setAdapter(cityAdapter);
 
         //init age menu
         final ListView ageView = new ListView(this);
@@ -74,26 +92,18 @@ public class MainActivity extends AppCompatActivity {
         ok.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mDropDownMenu.setTabText(constellationPosition == 0 ? headers[3] : constellations[constellationPosition]);
+                mDropDownMenu.setTabText(constellationPosition == 0 ? headers[2] : constellations[constellationPosition]);
                 mDropDownMenu.closeMenu();
             }
         });
 
         //init popupViews
-        popupViews.add(cityView);
+//        popupViews.add(cityView);
         popupViews.add(ageView);
         popupViews.add(sexView);
         popupViews.add(constellationView);
 
-        //add item click event
-        cityView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                cityAdapter.setCheckItem(position);
-                mDropDownMenu.setTabText(position == 0 ? headers[0] : citys[position]);
-                mDropDownMenu.closeMenu();
-            }
-        });
+//        //
 
         ageView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -122,14 +132,19 @@ public class MainActivity extends AppCompatActivity {
         });
 
         //init context view
-        TextView contentView = new TextView(this);
-        contentView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-        contentView.setText("内容显示区域");
-        contentView.setGravity(Gravity.CENTER);
-        contentView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
+//        contentView = new TextView(this);
+//        contentView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+//        contentView.setText("内容显示区域");
+//        contentView.setGravity(Gravity.CENTER);
+//        contentView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
 
         //init dropdownview
-        mDropDownMenu.setDropDownMenu(Arrays.asList(headers), popupViews, contentView);
+        allLl.removeView(topTv);
+        mDropDownMenu.setDropDownMenu(Arrays.asList(headers), popupViews, topTv);
+//        mDropDownMenuTop.setDropDownMenu(Arrays.asList(headers), popupViews, btn);
+
+
+
     }
 
     @Override
@@ -139,6 +154,34 @@ public class MainActivity extends AppCompatActivity {
             mDropDownMenu.closeMenu();
         } else {
             super.onBackPressed();
+        }
+        if (mDropDownMenuTop.isShowing()) {
+            mDropDownMenuTop.closeMenu();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private boolean isClick = true;
+
+    @OnClick(R.id.btn)
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btn:
+                if (isClick) {
+                    bottomLl.setVisibility(View.GONE);
+                    topLl.setVisibility(View.VISIBLE);
+                    isClick = false;
+                } else {
+                    bottomLl.setVisibility(View.VISIBLE);
+                    topLl.setVisibility(View.GONE);
+                    isClick = true;
+                }
+
+                break;
+
+            default:
+                break;
         }
     }
 }
